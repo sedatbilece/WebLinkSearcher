f948253955c58a367568988f276f6d8d72131a64
Sedat Bilece <seda1tbilece@gmail.com>
2023-02-20 15:06:09 +0300

    Add project files.

---
+++ WebSearcher/SearchItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebSearcher
+{
+    public class SearchItem
+    {
+
+        public string url { get; set; }
+        public string urlSection { get; set; }
+        public string Text { get; set; }
+    }
+}
