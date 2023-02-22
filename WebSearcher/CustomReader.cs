42ed1ece9a5fc70a23aab97d55ee07e316dfb301
Sedat Bilece <seda1tbilece@gmail.com>
2023-02-22 10:51:04 +0300

    refactor code

---
+++ WebSearcher/CustomReader.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WebSearcher
+{
+    public class CustomReader
+    {
+        private HttpClient _client { get; set; }
+
+        List<string> liste { get; set; } = new List<string>();
+        List<SearchItem> result { get; set; } = new List<SearchItem>();
+        public CustomReader()
+        {
+
+            _client = new HttpClient();
+        }
+
+
+
+
+
+        public async Task<List<string>> GetConnectionJsonAsync(string url)
+        {
+            string json = "";
+
+            try
+            {
+
+                var checkingResponse = await _client.GetAsync(url);
+
+                if (checkingResponse.IsSuccessStatusCode)
+                {
+                    liste.Add(checkingResponse.StatusCode.ToString());
+                }
+                else
+                {
+                    liste.Add(checkingResponse.StatusCode.ToString());
+                }
+                var stream = _client.GetStreamAsync(url).Result;
+                var reader = new StreamReader(stream);
+                json = reader.ReadToEnd();
+                reader.Close();
+
+            }
+            catch (Exception e)
+            {
+                liste.Add(e.Message);
+            }
+            liste.Add(json);
+
+            return liste;
+        }
+
+
+        public List<SearchItem> ReadJson(string inputString)
+        {
+            string hrefPattern = "<a\\s*(.*)\\>s*(.*)</a>";
+
+            try
+            {
+                Match regexMatch = Regex.Match(inputString, hrefPattern,
+                                               RegexOptions.None | RegexOptions.None,
+                                               TimeSpan.FromSeconds(1));
+                while (regexMatch.Success)
+                {
+                    if (regexMatch.Value.Contains("arifoglu.com"))
+                    {
+                        var list = regexMatch.Groups[1].Value.Split("//www.arifoglu.com/");
+                        var list2 = regexMatch.Groups[1].Value.Split($"href=\"");
+                        SearchItem search = new SearchItem();
+                        search.Text = regexMatch.Groups[2].Value;
+                        search.urlSection = list[1].Trim('"');
+                        search.url = list2[1].Trim('"');
+
+                        result.Add(search);
+                    }
+
+                    regexMatch = regexMatch.NextMatch();
+                }
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                Console.WriteLine("The matching operation timed out.");
+            }
+            return result;
+        }
+    }
+}
--- WebSearcher/Program.cs
+++ WebSearcher/Program.cs
@@ -7,79 +7,27 @@ using System.Reflection;
 
 var url = "https://teknobird.com/kaliteli-corek-otu-yagi/";
 string json = "";
-string IsSuccessStatusCode = "";
 
-try
-{
-    var client = new HttpClient();
+var customReader = new CustomReader();
 
-    var checkingResponse = await client.GetAsync(url);
-    if (checkingResponse.IsSuccessStatusCode)
-    {
-        Console.WriteLine("Bağlantı Başarılı ");
-        IsSuccessStatusCode= checkingResponse.IsSuccessStatusCode.ToString();
-        Console.WriteLine(IsSuccessStatusCode);
+var liste = await customReader.GetConnectionJsonAsync(url);
 
-    } else {
-        Console.WriteLine("Bağlantıda hata var");
-    }
-    var stream = client.GetStreamAsync(url).Result;
-    var reader = new StreamReader(stream);
-    json = reader.ReadToEnd();
-    reader.Close();
+var message = liste[0];
+Console.WriteLine("Status : " + message + "\n---------\n");
 
-}
-catch(Exception e)
-{
-    Console.WriteLine($"Error : {e.Message}");
-}
-
-
+json = liste[1];
 
 List<SearchItem> result = new List<SearchItem>();
 
- static void ReadJson(string inputString, List<SearchItem> result)
+
+result = customReader.ReadJson(json);
+
+
+foreach (var item in result)
 {
-    string hrefPattern = "<a\\s*(.*)\\>s*(.*)</a>";
-
-    try
-    {
-        Match regexMatch = Regex.Match(inputString, hrefPattern,
-                                       RegexOptions.None | RegexOptions.None,
-                                       TimeSpan.FromSeconds(1));
-        while (regexMatch.Success)
-        {
-            if (regexMatch.Value.Contains("arifoglu.com"))
-            {
-                var list = regexMatch.Groups[1].Value.Split("//www.arifoglu.com/");
-                var list2 = regexMatch.Groups[1].Value.Split($"href=\"");
-                SearchItem search = new SearchItem();
-                search.Text = regexMatch.Groups[2].Value;
-                search.urlSection = list[1].Trim('"');
-                search.url = list2[1].Trim('"');
-
-                result.Add(search);
-            }
-            
-            regexMatch = regexMatch.NextMatch();
-        }
-    }
-    catch (RegexMatchTimeoutException)
-    {
-        Console.WriteLine("The matching operation timed out.");
-    }
-}
-
-
-ReadJson(json, result);
-
-
-foreach(var item in result)
-{
-        Console.WriteLine(nameof(SearchItem.url) + " : " + item.url + "\n");
-        Console.WriteLine(nameof(SearchItem.urlSection) + " : " + item.urlSection + "\n");
-        Console.WriteLine(nameof(SearchItem.Text) + " : " + item.Text + "\n");
+    Console.WriteLine(nameof(SearchItem.url) + " : " + item.url + "\n");
+    Console.WriteLine(nameof(SearchItem.urlSection) + " : " + item.urlSection + "\n");
+    Console.WriteLine(nameof(SearchItem.Text) + " : " + item.Text + "\n");
 
     Console.WriteLine("---------\n");
-    }
-
+}
